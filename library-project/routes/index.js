14490f40cd6a025e03b52ec96cd8aed8cdf2979b
mjarraya <mjarraya@student.42.fr>
2019-04-15 12:11:47 +0200

    list and read documents

--- library-project/routes/index.js
+++ library-project/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Book = require('../models/book');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/books', (req, res) => {
+  Book.find({})
+    .then(books => {
+      res.render('books', { books });
+    })
+    .catch(err => {
+      console.error('Error while finding the books', err);
+    });
+});
+
+router.get('/books/:bookId', (req, res) => {
+  const _id = req.params.bookId;
+  Book.findOne({ _id })
+    .then(book => {
+      res.render('book-detail', { book });
+    })
+    .catch(err => {
+      console.error('Error while retrieving book with id ' + _id, err);
+    });
 });
 
 module.exports = router;
